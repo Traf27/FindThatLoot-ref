502394d01ea99cf58cbacb791f087be555f8b4b1
Piotr Szymaniec <traf@angrenost.org>
2020-05-27 22:33:15 +0200

    add wip mod JSON data format

---
+++ mods/mod json and class wip.ts
@@ -0,0 +1,46 @@
+{
+	"ilvl": 68,
+	"affixType": "Prefix",
+	"range": 
+	{
+		"from":
+	    	[
+	    		"min": 16,
+	    		"max": 20
+	    	],
+		"to": 
+	    	[
+	    		"min": 20,
+	    		"max": 25
+	    	],
+		"name": "%1 to %2 Cold Damage if you've dealt a Critical Strike Recently"		
+	},
+	"appearOnItem": [ "Jewel" ]
+}
+
+
+function fillModValues()
+
+
+
+class Mod {
+	ilvl: number;
+	affixType: string;
+	appearOnItem: string[""] 
+	range: ModRange
+}
+
+
+"range": 
+[
+"_1":	{
+		"min": 16,
+		"max": 20
+	},
+"_2": {
+		"min": 20,
+		"max": 25
+	}
+]
+
+
---
+++ mods/proposed format.txt.js
@@ -0,0 +1,58 @@
+	//{
+    //     ilvl: 68,
+    //     affixType: "Prefix",
+    //     <td>
+	   //      Adds 
+	   //      <span class="mod-value">(16–20)</span> 
+	   //      to 
+	   //      <span class="mod-value">(22–25)</span> 
+	   //      Cold Damage if you've dealt a Critical Strike Recently
+    //     </td>
+    //     <td>
+	   //      gloves_eyrie
+	   //      <span data-tooltip="500" class="glyphicon glyphicon-info-sign" data-hasqtip="2"></span>
+	   //      <br>
+	   //      <del>default</del>
+	   //      <span data-tooltip="0" class="glyphicon glyphicon-info-sign" data-hasqtip="3"></span>
+	   //      <br>
+    //     </td>
+    // },
+
+{
+	"ilvl": 68,
+	"affixType": "Prefix",
+	"range": 
+	{
+		"from":
+	    	[
+	    		"min": 16,
+	    		"max": 20
+	    	],
+		"to": 
+	    	[
+	    		"min": 20,
+	    		"max": 25
+	    	],
+		"name": "%1 to %2 Cold Damage if you've dealt a Critical Strike Recently"
+	//Recently 
+	}
+}
+        
+// mod.ilvl
+//mod.affixType
+
+// mod-value
+
+// 	        to 
+// 	        <span class="mod-value">(22–25)</span> 
+// 	        Cold Damage if you've dealt a Critical Strike Recently
+//         </td>
+//         <td>
+// 	        gloves_eyrie
+// 	        <span data-tooltip="500" class="glyphicon glyphicon-info-sign" data-hasqtip="2"></span>
+// 	        <br>
+// 	        <del>default</del>
+// 	        <span data-tooltip="0" class="glyphicon glyphicon-info-sign" data-hasqtip="3"></span>
+// 	        <br>
+//         </td>
+//    },
